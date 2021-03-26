cb5652eac2ff2c9409dd140d6e9dffdd6aefc3ff
Kjell W. Kongsvik <kjell.kongsvik@gmail.com>
2021-03-26 12:11:22 +0100

    Bump version: 0.3.0 → 0.4.0

--- api/api/__init__.py
+++ api/api/__init__.py
@@ -1,4 +1,4 @@
 """Geo:N:G - Data API"""
 
 # Version of Geo:N:G API
-__version__ = "0.3.0"  # This is automatically set using the bumpversion tool
+__version__ = "0.4.0"  # This is automatically set using the bumpversion tool
