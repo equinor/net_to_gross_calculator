f6204c3f9862f70b58b592c92ba57029ee1bc71f
Geir Arne Hjelle <geihj@equinor.com>
2021-04-20 09:46:27 +0200

    Bump version: 0.5.1 → 0.6.0

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -1,4 +1,4 @@
 """Geo:N:G - Quantitative reservoir predictor"""
 
 # Version of Geo:N:G
-__version__ = "0.5.1"  # This is automatically set using the bumpversion tool
+__version__ = "0.6.0"  # This is automatically set using the bumpversion tool
