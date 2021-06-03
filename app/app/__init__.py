842c182bb4dc674ab8ab8cd163228fadf63f9af9
Kjell W. Kongsvik <kjell.kongsvik@gmail.com>
2021-06-03 21:55:03 +0200

    Bump version: 1.0.0 → 1.1.0

--- app/app/__init__.py
+++ app/app/__init__.py
@@ -1,4 +1,4 @@
 """Geo:N:G - Quantitative reservoir predictor"""
 
 # Version of Geo:N:G
-__version__ = "1.0.0"  # This is automatically set using the bumpversion tool
+__version__ = "1.1.0"  # This is automatically set using the bumpversion tool
