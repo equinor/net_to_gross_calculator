eafef240d3ff67339ba867fbd9816d031a081202
Kjell W. Kongsvik <kjell.kongsvik@gmail.com>
2021-06-15 11:05:23 +0200

    Recreate tests certs, now valid for 10 years

--- tests/ssl/gen.sh
+++ tests/ssl/gen.sh
@@ -1,3 +1,3 @@
 openssl genrsa -out key.pem 4096
 # az
-openssl req -x509 -new -key key.pem -out az.pem -days 365 -subj '/CN=az' --nodes -addext "subjectAltName = DNS:az"
+openssl req -x509 -new -key key.pem -out az.pem -days 3650 -subj '/CN=az' --nodes -addext "subjectAltName = DNS:az"
